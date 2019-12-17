6b748e284981c9aafc1df8fd32a5fe47152e0b73
David Vossel <davidvossel@gmail.com>
2019-12-17 12:04:43 -0500

    automatically download golint if it doesn't exist
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+which golint
+if [ $? -ne 0 ]; then
+	echo "Downloading golint tool"
+	go get -u golang.org/x/lint/golint
+fi
+
 RETVAL=0
 GENERATED_FILES="zz_generated.*.go"
 for file in $(find . -path ./vendor -prune -o -type f -name '*.go' -print | grep -E -v "$GENERATED_FILES" | grep -E -v "functests"); do
