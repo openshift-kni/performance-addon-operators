45962d7cefa3e8931d4375315dc9cfaf7e3de37a
Artyom Lukianov <alukiano@redhat.com>
2019-12-22 16:43:01 +0200

    Provide topology manager tests
    - provide basic tests that will verify topology manager configuration
    - provide SR-IOV test, the kubelet should use the same NUMA node for SR-IOV and CPU's
    
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- functests/test_suite_test.go
+++ functests/test_suite_test.go
@@ -5,10 +5,18 @@ package test_test
 import (
 	"flag"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
+	_ "github.com/openshift-kni/performance-addon-operators/functests/performance" // this is needed otherwise the performance test won't be executed
+	testutils "github.com/openshift-kni/performance-addon-operators/functests/utils"
+	testclient "github.com/openshift-kni/performance-addon-operators/functests/utils/client"
+	"github.com/openshift-kni/performance-addon-operators/functests/utils/namespaces"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var junitPath *string
@@ -24,10 +32,21 @@ func TestTest(t *testing.T) {
 	if junitPath != nil {
 		rr = append(rr, reporters.NewJUnitReporter(*junitPath))
 	}
-	RunSpecsWithDefaultAndCustomReporters(t, "Test Suite", rr)
+	RunSpecsWithDefaultAndCustomReporters(t, "Performance Addon Operators e2e tests", rr)
 }
 
 var _ = BeforeSuite(func() {
-	//clients.Setup()
-	// Add here the setup for additional features
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testutils.NamespaceTesting,
+		},
+	}
+	_, err := testclient.Client.Namespaces().Create(ns)
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	err := testclient.Client.Namespaces().Delete(testutils.NamespaceTesting, &metav1.DeleteOptions{})
+	Expect(err).ToNot(HaveOccurred())
+	err = namespaces.WaitForDeletion(testclient.Client, testutils.NamespaceTesting, 5*time.Minute)
 })
