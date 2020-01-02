c3deb2c3c5cd54eb911cbe3e68e7bb4ad3ffec2a
Artyom Lukianov <alukiano@redhat.com>
2020-01-02 17:23:15 +0200

    Deploy the performance CR and prepare environment for testing
    - mark one of worker nodes with `worker-rt` role
    - wait for machine config pools updates
    
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- functests/test_suite_test.go
+++ functests/test_suite_test.go
@@ -4,6 +4,7 @@ package test_test
 
 import (
 	"flag"
+	"fmt"
 	"testing"
 	"time"
 
@@ -13,11 +14,17 @@ import (
 	_ "github.com/openshift-kni/performance-addon-operators/functests/performance" // this is needed otherwise the performance test won't be executed
 	testutils "github.com/openshift-kni/performance-addon-operators/functests/utils"
 	testclient "github.com/openshift-kni/performance-addon-operators/functests/utils/client"
+	"github.com/openshift-kni/performance-addon-operators/functests/utils/machineconfigpool"
 	"github.com/openshift-kni/performance-addon-operators/functests/utils/namespaces"
+	"github.com/openshift-kni/performance-addon-operators/functests/utils/nodes"
+	mcov1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+// TODO: we should refactor tests to use client from controller-runtime package
+// see - https://github.com/openshift/cluster-api-actuator-pkg/blob/master/pkg/e2e/framework/framework.go
 
 var junitPath *string
 
@@ -36,6 +43,7 @@ func TestTest(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
+	// create test namespace
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: testutils.NamespaceTesting,
@@ -43,6 +51,32 @@ var _ = BeforeSuite(func() {
 	}
 	_, err := testclient.Client.Namespaces().Create(ns)
 	Expect(err).ToNot(HaveOccurred())
+
+	// lable one of workers nodes with worker RT label
+	workerNodes, err := nodes.GetByRole(testclient.Client, testutils.RoleWorker)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(workerNodes).ToNot(BeEmpty())
+
+	worker := &workerNodes[0]
+	workerRTLabel := fmt.Sprintf("%s/%s", testutils.LabelRole, testutils.RoleWorkerRT)
+	worker.Labels[workerRTLabel] = ""
+	_, err = testclient.Client.Nodes().Update(worker)
+	Expect(err).ToNot(HaveOccurred())
+
+	// wait for all machine config pools to finish update
+	mcps, err := testclient.Client.MachineConfigPools().List(metav1.ListOptions{})
+	Expect(err).ToNot(HaveOccurred())
+
+	for _, mcp := range mcps.Items {
+		err := machineconfigpool.WaitForCondition(
+			testclient.Client,
+			&mcp,
+			mcov1.MachineConfigPoolUpdated,
+			corev1.ConditionTrue,
+			25*time.Minute,
+		)
+		Expect(err).ToNot(HaveOccurred())
+	}
 })
 
 var _ = AfterSuite(func() {
--- hack/clean-deploy.sh
+++ hack/clean-deploy.sh
@@ -3,6 +3,8 @@
 # expect oc to be in PATH by default
 OC_TOOL="${OC_TOOL:-oc}"
 
+$OC_TOOL delete performanceprofile --all
+
 $OC_TOOL delete ns openshift-performance-addon
 
 $OC_TOOL delete -f - <<EOF
