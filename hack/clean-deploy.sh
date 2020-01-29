e3bdcdc1b55e24646492c5b7fcaa18ecac5ac85d
shanks <gsr@redhat.com>
2020-01-29 21:19:42 +0530

    Remove worker-rt MCP with deploy clean-up
    Remove worker-rt MCP while performing 'make cluster-clean'

--- hack/clean-deploy.sh
+++ hack/clean-deploy.sh
@@ -8,3 +8,6 @@ $OC_TOOL delete performanceprofile --all
 $OC_TOOL delete ns openshift-performance-addon
 
 $OC_TOOL -n openshift-marketplace delete catalogsource performance-addon-operator-catalogsource
+
+$OC_TOOL delete mcp worker-rt
+
