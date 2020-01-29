363d78ad5e530b1240bea5428c380d45ce55dc59
Artyom Lukianov <alukiano@redhat.com>
2020-01-29 11:09:18 +0200

    Add unittests for hugepages allocation on the specific NUMA node
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- pkg/controller/performanceprofile/components/machineconfig/machineconfig_test.go
+++ pkg/controller/performanceprofile/components/machineconfig/machineconfig_test.go
@@ -3,6 +3,8 @@ package machineconfig
 import (
 	"fmt"
 
+	"k8s.io/utils/pointer"
+
 	"github.com/ghodss/yaml"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -47,6 +49,27 @@ const expectedSystemdUnits = `
         enabled: true
         name: reboot.service
 `
+
+const hugepagesAllocationService = `
+      - contents: |
+          [Unit]
+          Description=Hugepages-1048576kB allocation on the node 0
+          Before=kubelet.service
+
+          [Service]
+          Environment=HUGEPAGES_COUNT=4
+          Environment=HUGEPAGES_SIZE=1048576
+          Environment=NUMA_NODE=0
+          Type=oneshot
+          RemainAfterExit=true
+          ExecStart=/usr/local/bin/hugepages-allocation.sh
+
+          [Install]
+          WantedBy=multi-user.target
+        enabled: true
+        name: hugepages-allocation-1048576kB-NUMA0.service
+`
+
 const expectedBootArguments = `
   kernelArguments:
   - nohz=on
@@ -91,5 +114,34 @@ var _ = Describe("Machine Config", func() {
 		Expect(manifest).To(ContainSubstring(fmt.Sprintf("%s: %s", labelKey, labelValue)))
 		Expect(manifest).To(ContainSubstring(expectedSystemdUnits))
 		Expect(manifest).To(ContainSubstring(expectedBootArguments))
+	})
+
+	Context("with hugepages with specified NUMA node", func() {
+		var manifest string
+
+		BeforeEach(func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Spec.HugePages.Pages[0].Node = pointer.Int32Ptr(0)
+
+			mc, err := New(testAssetsDir, profile)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(mc.Spec.KernelType).To(Equal(mcKernelRT))
+
+			y, err := yaml.Marshal(mc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest = string(y)
+			labelKey, labelValue := components.GetFirstKeyAndValue(profile.Spec.MachineConfigLabel)
+			Expect(manifest).To(ContainSubstring(fmt.Sprintf("%s: %s", labelKey, labelValue)))
+		})
+
+		It("should not add hugepages kernel boot parameters", func() {
+			Expect(manifest).ToNot(ContainSubstring("- hugepagesz=1G"))
+			Expect(manifest).ToNot(ContainSubstring("- hugepages=4"))
+		})
+
+		It("should add systemd unit to allocate hugepages", func() {
+			Expect(manifest).To(ContainSubstring(hugepagesAllocationService))
+		})
 	})
 })
