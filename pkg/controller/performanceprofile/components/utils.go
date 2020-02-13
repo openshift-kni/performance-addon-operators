a181a363fba5b04763350b55b8e03b91caa16da8
Yanir Quinn <yquinn@redhat.com>
2020-02-13 10:43:44 +0200

    Added cpu calculations

--- pkg/controller/performanceprofile/components/utils.go
+++ pkg/controller/performanceprofile/components/utils.go
@@ -2,6 +2,8 @@ package components
 
 import (
 	"fmt"
+	"math/big"
+	"strconv"
 	"strings"
 )
 
@@ -27,15 +29,64 @@ func SplitLabelKey(s string) (domain, role string, err error) {
 	return parts[0], parts[1], nil
 }
 
+func cpusListToArray(cpusListStr string) ([]int, error) {
+	var cpusList []int
+	elements := strings.Split(cpusListStr, ",")
+	for _, item := range elements {
+		cpuRange := strings.Split(item, "-")
+		// provided a range: 1-3
+		if len(cpuRange) > 1 {
+			start, err := strconv.Atoi(cpuRange[0])
+			if err != nil {
+				return nil, err
+			}
+			end, err := strconv.Atoi(cpuRange[1])
+			if err != nil {
+				return nil, err
+			}
+			// Add cpus to the list. Assuming it's a valid range.
+			for cpuNum := start; cpuNum <= end; cpuNum++ {
+				cpusList = append(cpusList, cpuNum)
+			}
+		} else {
+			cpuNum, err := strconv.Atoi(cpuRange[0])
+			if err != nil {
+				return nil, err
+			}
+			cpusList = append(cpusList, cpuNum)
+		}
+	}
+
+	return cpusList, nil
+}
+
 // CPUListToHexMask converts a list of cpus into a cpu mask represented in hexdecimal
 func CPUListToHexMask(cpulist string) (hexMask string) {
-	return ""
+	reservedCpus, _ := cpusListToArray(cpulist)
+	currMask := big.NewInt(0)
+	for _, cpu := range reservedCpus {
+		x := new(big.Int).Lsh(big.NewInt(1), uint(cpu))
+		currMask.Or(currMask, x)
+	}
+	return fmt.Sprintf("%02x", currMask)
 }
 
 // CPUListToInvertedMask converts a list of cpus into an inverted cpu mask represented in hexdecimal
 func CPUListToInvertedMask(cpulist string) (hexMask string) {
-	return ""
+	reservedCpus, _ := cpusListToArray(cpulist)
+
+	reservedCpusLookup := make(map[int]bool)
+	for _, cpu := range reservedCpus {
+		reservedCpusLookup[cpu] = true
+	}
+
+	currMask := big.NewInt(0)
+	for cpu := 0; cpu < 254; cpu++ {
+		if _, reserved := reservedCpusLookup[cpu]; reserved {
+			continue
+		}
+		x := new(big.Int).Lsh(big.NewInt(1), uint(cpu))
+		currMask.Or(currMask, x)
+	}
+	return fmt.Sprintf("%02x", currMask)
 }
-
-
-
