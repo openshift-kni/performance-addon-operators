02361aa01dd6765e9b3d37a67ed2eb68a1571c17
Yanir Quinn <yquinn@redhat.com>
2020-02-27 12:23:14 +0200

    Add check for conf file injection in script

--- build/assets/scripts/pre-boot-tuning.sh
+++ build/assets/scripts/pre-boot-tuning.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SYSTEM_CONFIG_FILE="/etc/systemd/system.conf"
 SYSTEM_CONFIG_CUSTOM_FILE="/etc/systemd/system.conf.d/setAffinity.conf"
 
-if [ -f /etc/sysconfig/irqbalance ] && [ -f ${SYSTEM_CONFIG_CUSTOM_FILE} ] && egrep -wq "^IRQBALANCE_BANNED_CPUS=${RESERVED_CPU_MASK_INVERT}" /etc/sysconfig/irqbalance; then
+if [ -f /etc/sysconfig/irqbalance ] && [ -f ${SYSTEM_CONFIG_CUSTOM_FILE} ] && rpm-ostree status -b | grep -q -e '-I /etc/systemd/system.conf /etc/systemd/system.conf.d/setAffinity.conf' && egrep -wq "^IRQBALANCE_BANNED_CPUS=${RESERVED_CPU_MASK_INVERT}" /etc/sysconfig/irqbalance; then
     echo "Pre boot tuning configuration already applied"
     echo "Setting kernel rcuo* threads to the housekeeping cpus"
     pgrep rcuo* | while read line; do taskset -pc ${RESERVED_CPUS} $line || true; done
@@ -20,6 +20,11 @@ else
     else
         echo "IRQBALANCE_BANNED_CPUS=${RESERVED_CPU_MASK_INVERT}" >>/etc/sysconfig/irqbalance
     fi
+
+    if [[ ! -f /var/restarts ]]; then
+        echo 0 > /var/restarts
+    fi
+    echo "$(expr $(cat /var/restarts) + 1)" > /var/restarts
 
     rpm-ostree initramfs -r --enable --arg=-I --arg="${SYSTEM_CONFIG_FILE} ${SYSTEM_CONFIG_CUSTOM_FILE}" 
 fi
