8b8475b1c7e1b2e82d17528def0594bbf15dc012
Denys Shchedrivyi <dshchedr@redhat.com>
2020-03-05 09:36:23 -0500

    Add functional tests - updating performance profile

--- hack/run-functests.sh
+++ hack/run-functests.sh
@@ -6,4 +6,7 @@ if [ $? -ne 0 ]; then
 	go install github.com/onsi/ginkgo/ginkgo
 fi
 
-GOFLAGS=-mod=vendor ginkgo functests -- -junit /tmp/artifacts/unit_report.xml
+# after running "updating profile" tests - some other functional tests might be failed, so it's better to run them separately
+GOFLAGS=-mod=vendor ginkgo functests -- -junit /tmp/artifacts/unit_report.xml -ginkgo.skip "Updating parameters in performance profile"
+GOFLAGS=-mod=vendor ginkgo functests -- -junit /tmp/artifacts/updating_profile_unit_report.xml -ginkgo.focus "Updating parameters in performance profile"
+
