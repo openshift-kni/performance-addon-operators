f94f129b76b66a175cbc7e22fecd788c9f46a3e4
Francesco Romani <fromani@redhat.com>
2020-05-06 12:14:29 +0200

    cleanup: streamline and use csv-generator options
    This patch renames for consistency the `-inject-annotations-from`
    option to `-annotations-from` - so all the options now look like
    `-*-from`. We fix all the usages in the source tree.
    
    Additionally, let's also use in the generator script the option
    to replace the CSV description. This is opt-in as the rest of the
    replacement options.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- tools/csv-generator/csv-generator.go
+++ tools/csv-generator/csv-generator.go
@@ -31,7 +31,7 @@ var (
 
 	outputDir = flag.String("olm-bundle-directory", "", "The directory to output the unified CSV and CRDs to")
 
-	annotationsFile = flag.String("inject-annotations-from", "", "inject metadata annotations from given file")
+	annotationsFile = flag.String("annotations-from", "", "add metadata annotations from given file")
 	maintainersFile = flag.String("maintainers-from", "", "add maintainers list from given file")
 	descriptionFile = flag.String("description-from", "", "replace the description with the content of the given file")
 
