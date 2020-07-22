e2aa775b102aa107c5c3fe82925c2c0a3086ea51
Marcel Apfelbaum <marcel@redhat.com>
2020-07-22 15:32:38 +0300

    hack/docs: Point the docs generator to v1 API
    There is no visible change to the API, but
    it seems like the right think to do.
    
    Delay the generation of docs for both APIs until
    the first change in the API.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>

--- hack/docs-generate.sh
+++ hack/docs-generate.sh
@@ -4,7 +4,7 @@ set -e
 
 export GOROOT=$(go env GOROOT)
 
-export PERF_PROFILE_TYPES=pkg/apis/performance/v1alpha1/performanceprofile_types.go
+export PERF_PROFILE_TYPES=pkg/apis/performance/v1/performanceprofile_types.go
 export PERF_PROFILE_DOC=docs/performance_profile.md
 
 # using the generated CSV, create the real CSV by injecting all the right data into it
