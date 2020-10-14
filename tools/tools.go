a7e3f49839ac914e2e1bc81f65e67ab3e4599995
Marcel Apfelbaum <marcel@redhat.com>
2020-10-14 11:02:15 +0300

    tools: Add a dummy file to force controller-gen vendoring
    Is a dummy go file that will be ignored for builds, but included for dependencies.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>

---
+++ tools/tools.go
@@ -0,0 +1,8 @@
+//+build main
+
+package main
+
+// A dummy go file that will be ignored for builds, but included for dependencies.
+import (
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+)
