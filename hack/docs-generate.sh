b01438b22900056892a32521dd95b80f45258086
Marcel Apfelbaum <marcel@redhat.com>
2020-10-20 18:51:53 +0300

    hack/docs: Point the docs generator to Performance Profile v2 API
    Generate docs only for the v2 API.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>

--- hack/docs-generate.sh
+++ hack/docs-generate.sh
@@ -4,7 +4,7 @@ set -e
 
 export GOROOT=$(go env GOROOT)
 
-export PERF_PROFILE_TYPES=api/v1/performanceprofile_types.go
+export PERF_PROFILE_TYPES=api/v2/performanceprofile_types.go
 export PERF_PROFILE_DOC=docs/performance_profile.md
 
 # using the generated CSV, create the real CSV by injecting all the right data into it
