d488acc13156e69570c5c816d2dc619b3614c647
Francesco Romani <fromani@redhat.com>
2020-11-05 15:39:22 +0100

    tests: fix default namespace, and allow override
    Always use the namespaces suggested in the official documentation.
    
    Allow to override the namespace name the tests wants to consume;
    this is needed because when doing validation, we need to make sure to
    check what was actually installed in the cluster, and there is no hard
    requirement on the namespace name - only suggestions.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- functests/utils/namespaces/namespaces.go
+++ functests/utils/namespaces/namespaces.go
@@ -2,6 +2,7 @@ package namespaces
 
 import (
 	"context"
+	"os"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -13,6 +14,17 @@ import (
 	testutils "github.com/openshift-kni/performance-addon-operators/functests/utils"
 	testclient "github.com/openshift-kni/performance-addon-operators/functests/utils/client"
 )
+
+// PerformanceOperator contains the name of the performance operator namespace
+// default as recommended in
+// https://docs.openshift.com/container-platform/4.6/scalability_and_performance/cnf-performance-addon-operator-for-low-latency-nodes.html#install-operator-cli_cnf-master
+var PerformanceOperator string = "openshift-performance-addon-operator"
+
+func init() {
+	if operatorNS, ok := os.LookupEnv("PERFORMANCE_OPERATOR_NAMESPACE"); ok {
+		PerformanceOperator = operatorNS
+	}
+}
 
 // TestingNamespace is the namespace the tests will use for running test pods
 var TestingNamespace = &corev1.Namespace{
