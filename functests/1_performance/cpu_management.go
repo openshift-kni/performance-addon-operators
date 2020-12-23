8a66ff5ebd08ef59404e457001dc5d543b27d5c9
Niranjan M.R <mrniranjan@redhat.com>
2020-12-23 15:52:52 +0530

    Updating Polarion test id for CPU Affinity and Isolation on Non-Guaranteed Pod
    Add a new polarion test case id for test case related to
    test case : Check CPU Affinity and Isolation on Not-Guaranteed QoS class pod
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>

--- functests/1_performance/cpu_management.go
+++ functests/1_performance/cpu_management.go
@@ -146,7 +146,7 @@ var _ = Describe("[rfe_id:27363][performance] CPU Management", func() {
 		})
 	})
 
-	Describe("[test_id:27492][crit:high][vendor:cnf-qe@redhat.com][level:acceptance] Verification of cpu manager functionality", func() {
+	Describe("Verification of cpu manager functionality", func() {
 		var testpod *corev1.Pod
 		var discoveryFailed bool
 
@@ -215,8 +215,8 @@ var _ = Describe("[rfe_id:27363][performance] CPU Management", func() {
 
 			Expect(cpu).To(BeElementOf(listCPU))
 		},
-			table.Entry("Non-guaranteed POD can work on any CPU", false),
-			table.Entry("Guaranteed POD should work on isolated cpu", true),
+			table.Entry("[test_id:37860] Non-guaranteed POD can work on any CPU", false),
+			table.Entry("[test_id:27492] Guaranteed POD should work on isolated cpu", true),
 		)
 	})
 
