7a27c85edb2f19569506ddc8edbf24e6a968196e
Marcel Apfelbaum <marcel@redhat.com>
2021-03-03 18:16:40 +0200

    pkg/profilecreator: Update the unit tests to use a const must-gather dir
    Use a const for the must-gather path.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>

--- pkg/profilecreator/profilecreator_test.go
+++ pkg/profilecreator/profilecreator_test.go
@@ -12,6 +12,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
+)
+
 var _ = Describe("PerformanceProfileCreator: MCP and Node Matching", func() {
 	var node *v1.Node
 	var labelSelector *metav1.LabelSelector
@@ -47,13 +51,12 @@ var _ = Describe("PerformanceProfileCreator: MCP and Node Matching", func() {
 })
 
 var _ = Describe("PerformanceProfileCreator: Getting MCP from Must Gather", func() {
-	var mustGatherDirPath, mcpName, mcpNodeSelectorKey, mustGatherDirAbsolutePath string
+	var mcpName, mcpNodeSelectorKey, mustGatherDirAbsolutePath string
 	var err error
 	Context("Identifying Nodes targetted by MCP", func() {
 		It("gets the MCP successfully", func() {
 			mcpName = "worker-cnf"
 			mcpNodeSelectorKey = "node-role.kubernetes.io/worker-cnf"
-			mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
 			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
 			Expect(err).ToNot(HaveOccurred())
 			mcp, err := GetMCP(mustGatherDirAbsolutePath, mcpName)
@@ -63,7 +66,6 @@ var _ = Describe("PerformanceProfileCreator: Getting MCP from Must Gather", func
 		})
 		It("fails to get MCP as an MCP with that name doesn't exist", func() {
 			mcpName = "foo"
-			mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
 			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
 			mcp, err := GetMCP(mustGatherDirAbsolutePath, mcpName)
 			Expect(mcp).To(BeNil())
@@ -71,8 +73,7 @@ var _ = Describe("PerformanceProfileCreator: Getting MCP from Must Gather", func
 		})
 		It("fails to get MCP due to misconfigured must-gather path", func() {
 			mcpName = "worker-cnf"
-			mustGatherDirPath = "../../testdata/must-gather/foo-path"
-			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
+			mustGatherDirAbsolutePath, err = filepath.Abs("foo-path")
 			Expect(err).ToNot(HaveOccurred())
 			_, err := GetMCP(mustGatherDirAbsolutePath, mcpName)
 			Expect(err).To(HaveOccurred())
@@ -82,12 +83,11 @@ var _ = Describe("PerformanceProfileCreator: Getting MCP from Must Gather", func
 })
 
 var _ = Describe("PerformanceProfileCreator: Getting Nodes from Must Gather", func() {
-	var mustGatherDirPath, mustGatherDirAbsolutePath string
+	var mustGatherDirAbsolutePath string
 	var err error
 
 	Context("Identifying Nodes in the cluster", func() {
 		It("gets the Nodes successfully", func() {
-			mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
 			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
 			Expect(err).ToNot(HaveOccurred())
 			nodes, err := GetNodeList(mustGatherDirAbsolutePath)
@@ -95,8 +95,7 @@ var _ = Describe("PerformanceProfileCreator: Getting Nodes from Must Gather", fu
 			Expect(len(nodes)).To(Equal(5))
 		})
 		It("fails to get Nodes due to misconfigured must-gather path", func() {
-			mustGatherDirPath = "../../testdata/must-gather/foo-path"
-			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
+			mustGatherDirAbsolutePath, err = filepath.Abs("foo-path")
 			_, err := GetNodeList(mustGatherDirAbsolutePath)
 			Expect(err).To(HaveOccurred())
 		})
@@ -105,14 +104,13 @@ var _ = Describe("PerformanceProfileCreator: Getting Nodes from Must Gather", fu
 })
 
 var _ = Describe("PerformanceProfileCreator: Consuming GHW Snapshot from Must Gather", func() {
-	var mustGatherDirPath, mustGatherDirAbsolutePath string
+	var mustGatherDirAbsolutePath string
 	var node *v1.Node
 	var err error
 
 	Context("Identifying Nodes Info of the nodes cluster", func() {
 		It("gets the Nodes Info successfully", func() {
 			node = newTestNode("cnfd1-worker-0.fci1.kni.lab.eng.bos.redhat.com")
-			mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
 			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
 			Expect(err).ToNot(HaveOccurred())
 			handle, err := NewGHWHandler(mustGatherDirAbsolutePath, node)
@@ -127,14 +125,12 @@ var _ = Describe("PerformanceProfileCreator: Consuming GHW Snapshot from Must Ga
 			Expect(len(topologyInfo.Nodes)).To(Equal(2))
 		})
 		It("fails to get Nodes Info due to misconfigured must-gather path", func() {
-			mustGatherDirPath = "../../testdata/must-gather/foo-path"
-			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
+			mustGatherDirAbsolutePath, err = filepath.Abs("foo-path")
 			_, err := NewGHWHandler(mustGatherDirAbsolutePath, node)
 			Expect(err).To(HaveOccurred())
 		})
 		It("fails to get Nodes Info for a node that does not exist", func() {
 			node = newTestNode("foo")
-			mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
 			mustGatherDirAbsolutePath, err = filepath.Abs(mustGatherDirPath)
 			_, err := NewGHWHandler(mustGatherDirAbsolutePath, node)
 			Expect(err).To(HaveOccurred())
@@ -144,7 +140,7 @@ var _ = Describe("PerformanceProfileCreator: Consuming GHW Snapshot from Must Ga
 })
 
 var _ = Describe("PerformanceProfileCreator: Populating Reserved and Isolated CPUs in Performance Profile", func() {
-	var mustGatherDirPath, mustGatherDirAbsolutePath string
+	var mustGatherDirAbsolutePath string
 	var node *v1.Node
 	var handle *GHWHandler
 	var splitReservedCPUsAcrossNUMA bool
@@ -153,7 +149,6 @@ var _ = Describe("PerformanceProfileCreator: Populating Reserved and Isolated CP
 
 	BeforeEach(func() {
 		node = newTestNode("cnfd1-worker-0.fci1.kni.lab.eng.bos.redhat.com")
-		mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
 	})
 	Context("Check if reserved and isolated CPUs are properly populated in the performance profile", func() {
 		It("Ensure reserved CPUs populated are correctly when splitReservedCPUsAcrossNUMA is disabled", func() {
@@ -215,14 +210,11 @@ var _ = Describe("PerformanceProfileCreator: Populating Reserved and Isolated CP
 })
 
 var _ = Describe("PerformanceProfileCreator: Check if Hyperthreading enabled/disabled in a system to correctly populate reserved and isolated CPUs in the performance profile", func() {
-	var mustGatherDirPath, mustGatherDirAbsolutePath string
+	var mustGatherDirAbsolutePath string
 	var node *v1.Node
 	var handle *GHWHandler
 	var err error
 
-	BeforeEach(func() {
-		mustGatherDirPath = "../../testdata/must-gather/must-gather.local.directory"
-	})
 	Context("Check if hyperthreading is enabled on the system or not", func() {
 		It("Ensure we detect correctly that hyperthreading is enabled on a system", func() {
 			node = newTestNode("cnfd1-worker-0.fci1.kni.lab.eng.bos.redhat.com")
