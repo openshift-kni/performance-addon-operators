76aa817252b817e77f57b4319244b1ded4ca74da
Artyom Lukianov <alukiano@redhat.com>
2021-08-01 17:36:26 +0300

    controller: use the kubelet config snippet annotation during KubeletConfig creation
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- pkg/controller/performanceprofile/components/kubeletconfig/kubeletconfig_test.go
+++ pkg/controller/performanceprofile/components/kubeletconfig/kubeletconfig_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/ghodss/yaml"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
+	"k8s.io/utils/pointer"
 
 	"github.com/openshift-kni/performance-addon-operators/pkg/controller/performanceprofile/components"
 	testutils "github.com/openshift-kni/performance-addon-operators/pkg/utils/testing"
-	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
-	"k8s.io/utils/pointer"
 )
 
 const testReservedMemory = `reservedMemory:
@@ -69,6 +69,69 @@ var _ = Describe("Kubelet Config", func() {
 			manifest := string(y)
 			Expect(manifest).ToNot(ContainSubstring("memoryManagerPolicy: Static"))
 			Expect(manifest).ToNot(ContainSubstring(testReservedMemory))
+		})
+	})
+
+	Context("with additional kubelet arguments", func() {
+		It("should not override CPU manager parameters", func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"cpuManagerPolicy": "none", "cpuManagerReconcilePeriod": "10s", "reservedSystemCPUs": "4,5"}`,
+			}
+			selectorKey, selectorValue := components.GetFirstKeyAndValue(profile.Spec.MachineConfigPoolSelector)
+			kc, err := New(profile, map[string]string{selectorKey: selectorValue})
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).ToNot(ContainSubstring("cpuManagerPolicy: none"))
+			Expect(manifest).ToNot(ContainSubstring("cpuManagerReconcilePeriod: 10s"))
+			Expect(manifest).ToNot(ContainSubstring("reservedSystemCPUs: 4-5"))
+		})
+
+		It("should not override topology manager parameters", func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"topologyManagerPolicy": "none"}`,
+			}
+			selectorKey, selectorValue := components.GetFirstKeyAndValue(profile.Spec.MachineConfigPoolSelector)
+			kc, err := New(profile, map[string]string{selectorKey: selectorValue})
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).ToNot(ContainSubstring("topologyManagerPolicy: none"))
+		})
+
+		It("should not override memory manager policy", func() {
+			profile := testutils.NewPerformanceProfile("test")
+
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"memoryManagerPolicy": "None", "reservedMemory": [{"numaNode": 10, "limits": {"test": "1024"}}]}`,
+			}
+			selectorKey, selectorValue := components.GetFirstKeyAndValue(profile.Spec.MachineConfigPoolSelector)
+			kc, err := New(profile, map[string]string{selectorKey: selectorValue})
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).ToNot(ContainSubstring("memoryManagerPolicy: None"))
+			Expect(manifest).To(ContainSubstring("numaNode: 10"))
+		})
+
+		It("should set the kubelet config accordingly", func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"allowedUnsafeSysctls": ["net.core.somaxconn"], "evictionHard": {"memory.available": "200Mi"}}`,
+			}
+			selectorKey, selectorValue := components.GetFirstKeyAndValue(profile.Spec.MachineConfigPoolSelector)
+			kc, err := New(profile, map[string]string{selectorKey: selectorValue})
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).To(ContainSubstring("net.core.somaxconn"))
+			Expect(manifest).To(ContainSubstring("memory.available: 200Mi"))
 		})
 	})
 })
