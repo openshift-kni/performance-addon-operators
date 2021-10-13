e88db7b29fb29b549688c57a0a21f9e0d88bedcf
OpenShift Merge Robot <openshift-merge-robot@users.noreply.github.com>
2021-10-13 03:02:08 +0200

    Merge pull request #754 from openshift-cherrypick-robot/cherry-pick-708-to-release-4.9
    [release-4.9] Bug 2013216: provide a way to configure additional kubelet arguments
    
    (cherry picked from commit c69d3f37bb5096c1806ca263b06b0196f1505c33)
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- pkg/controller/performanceprofile/components/kubeletconfig/kubeletconfig.go
+++ pkg/controller/performanceprofile/components/kubeletconfig/kubeletconfig.go
@@ -4,43 +4,77 @@ import (
 	"encoding/json"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
+
 	performancev2 "github.com/openshift-kni/performance-addon-operators/api/v2"
 	"github.com/openshift-kni/performance-addon-operators/pkg/controller/performanceprofile/components"
 	profile2 "github.com/openshift-kni/performance-addon-operators/pkg/controller/performanceprofile/components/profile"
 	machineconfigv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
 )
 
 const (
-	cpuManagerPolicyStatic      = "static"
-	defaultKubeReservedCPU      = "1000m"
-	defaultKubeReservedMemory   = "500Mi"
-	defaultSystemReservedCPU    = "1000m"
-	defaultSystemReservedMemory = "500Mi"
+	// experimentalKubeletSnippetAnnotation contains the annotation key that should be used to provide a KubeletConfig snippet with additional
+	// configurations you want to apply on top of the generated KubeletConfig resource.
+	// To find the specific argument see https://kubernetes.io/docs/reference/config-api/kubelet-config.v1beta1/.
+	// By default, the performance-addon-operator will override:
+	// 1. CPU manager policy
+	// 2. CPU manager reconcile period
+	// 3. Topology manager policy
+	// 4. Reserved CPUs
+	// 5. Memory manager policy
+	// Please avoid specifying them and use the relevant API to configure these parameters.
+	experimentalKubeletSnippetAnnotation = "kubeletconfig.experimental"
+	cpuManagerPolicyStatic               = "static"
+	defaultKubeReservedMemory            = "500Mi"
+	defaultSystemReservedMemory          = "500Mi"
+	defaultHardEvictionThreshold         = "100Mi"
+	evictionHardMemoryAvailable          = "memory.available"
 )
 
 // New returns new KubeletConfig object for performance sensetive workflows
 func New(profile *performancev2.PerformanceProfile) (*machineconfigv1.KubeletConfig, error) {
 	name := components.GetComponentName(profile.Name, components.ComponentNamePrefix)
-	kubeletConfig := &kubeletconfigv1beta1.KubeletConfiguration{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "kubelet.config.k8s.io/v1beta1",
-			Kind:       "KubeletConfiguration",
-		},
-		CPUManagerPolicy:          cpuManagerPolicyStatic,
-		CPUManagerReconcilePeriod: metav1.Duration{Duration: 5 * time.Second},
-		TopologyManagerPolicy:     kubeletconfigv1beta1.BestEffortTopologyManagerPolicy,
-		KubeReserved: map[string]string{
-			"cpu":    defaultKubeReservedCPU,
-			"memory": defaultKubeReservedMemory,
-		},
-		SystemReserved: map[string]string{
-			"cpu":    defaultSystemReservedCPU,
-			"memory": defaultSystemReservedMemory,
-		},
+	kubeletConfig := &kubeletconfigv1beta1.KubeletConfiguration{}
+	if v, ok := profile.Annotations[experimentalKubeletSnippetAnnotation]; ok {
+		if err := json.Unmarshal([]byte(v), kubeletConfig); err != nil {
+			return nil, err
+		}
+	}
+
+	kubeletConfig.TypeMeta = metav1.TypeMeta{
+		APIVersion: kubeletconfigv1beta1.SchemeGroupVersion.String(),
+		Kind:       "KubeletConfiguration",
+	}
+
+	kubeletConfig.CPUManagerPolicy = cpuManagerPolicyStatic
+	kubeletConfig.CPUManagerReconcilePeriod = metav1.Duration{Duration: 5 * time.Second}
+	kubeletConfig.TopologyManagerPolicy = kubeletconfigv1beta1.BestEffortTopologyManagerPolicy
+
+	// set the default hard eviction memory threshold
+	if kubeletConfig.EvictionHard == nil {
+		kubeletConfig.EvictionHard = map[string]string{}
+	}
+	if _, ok := kubeletConfig.EvictionHard[evictionHardMemoryAvailable]; !ok {
+		kubeletConfig.EvictionHard[evictionHardMemoryAvailable] = defaultHardEvictionThreshold
+	}
+
+	// set the default memory kube-reserved
+	if kubeletConfig.KubeReserved == nil {
+		kubeletConfig.KubeReserved = map[string]string{}
+	}
+	if _, ok := kubeletConfig.KubeReserved[string(corev1.ResourceMemory)]; !ok {
+		kubeletConfig.KubeReserved[string(corev1.ResourceMemory)] = defaultKubeReservedMemory
+	}
+
+	// set the default memory system-reserved
+	if kubeletConfig.SystemReserved == nil {
+		kubeletConfig.SystemReserved = map[string]string{}
+	}
+	if _, ok := kubeletConfig.SystemReserved[string(corev1.ResourceMemory)]; !ok {
+		kubeletConfig.SystemReserved[string(corev1.ResourceMemory)] = defaultSystemReservedMemory
 	}
 
 	if profile.Spec.CPU != nil && profile.Spec.CPU.Reserved != nil {
@@ -49,7 +83,7 @@ func New(profile *performancev2.PerformanceProfile) (*machineconfigv1.KubeletCon
 
 	if profile.Spec.NUMA != nil {
 		if profile.Spec.NUMA.TopologyPolicy != nil {
-			kubeletConfig.TopologyManagerPolicy = string(*profile.Spec.NUMA.TopologyPolicy)
+			kubeletConfig.TopologyManagerPolicy = *profile.Spec.NUMA.TopologyPolicy
 		}
 	}
 
--- pkg/controller/performanceprofile/components/kubeletconfig/kubeletconfig_test.go
+++ pkg/controller/performanceprofile/components/kubeletconfig/kubeletconfig_test.go
@@ -28,4 +28,48 @@ var _ = Describe("Kubelet Config", func() {
 		Expect(manifest).To(ContainSubstring("topologyManagerPolicy: single-numa-node"))
 		Expect(manifest).To(ContainSubstring("cpuManagerPolicy: static"))
 	})
+
+	Context("with additional kubelet arguments", func() {
+		It("should not override CPU manager parameters", func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"cpuManagerPolicy": "none", "cpuManagerReconcilePeriod": "10s", "reservedSystemCPUs": "4,5"}`,
+			}
+			kc, err := New(profile)
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).ToNot(ContainSubstring("cpuManagerPolicy: none"))
+			Expect(manifest).ToNot(ContainSubstring("cpuManagerReconcilePeriod: 10s"))
+			Expect(manifest).ToNot(ContainSubstring("reservedSystemCPUs: 4-5"))
+		})
+
+		It("should not override topology manager parameters", func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"topologyManagerPolicy": "none"}`,
+			}
+			kc, err := New(profile)
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).ToNot(ContainSubstring("topologyManagerPolicy: none"))
+		})
+
+		It("should set the kubelet config accordingly", func() {
+			profile := testutils.NewPerformanceProfile("test")
+			profile.Annotations = map[string]string{
+				experimentalKubeletSnippetAnnotation: `{"allowedUnsafeSysctls": ["net.core.somaxconn"], "evictionHard": {"memory.available": "200Mi"}}`,
+			}
+			kc, err := New(profile)
+			y, err := yaml.Marshal(kc)
+			Expect(err).ToNot(HaveOccurred())
+
+			manifest := string(y)
+			Expect(manifest).To(ContainSubstring("net.core.somaxconn"))
+			Expect(manifest).To(ContainSubstring("memory.available: 200Mi"))
+		})
+	})
 })
