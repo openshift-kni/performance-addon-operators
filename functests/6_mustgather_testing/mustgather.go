b7a2967c4a4120d3a3a6ef32b119cfd24695ecca
Niranjan M.R <mrniranjan@redhat.com>
2022-05-04 22:47:03 +0530

    Minor fixes: Check PAO profile and pod
    Verify PAO profile and Pod before asserting
    PAO related files
    
    Get mcps using machineconfig API
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>

--- functests/6_mustgather_testing/mustgather.go
+++ functests/6_mustgather_testing/mustgather.go
@@ -1,6 +1,7 @@
 package pao_mustgather
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,12 +9,16 @@ import (
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	performancev2 "github.com/openshift-kni/performance-addon-operators/api/v2"
 	testutils "github.com/openshift-kni/performance-addon-operators/functests/utils"
+	testclient "github.com/openshift-kni/performance-addon-operators/functests/utils/client"
 	"github.com/openshift-kni/performance-addon-operators/functests/utils/nodes"
 	"github.com/openshift-kni/performance-addon-operators/functests/utils/pods"
 	"github.com/openshift-kni/performance-addon-operators/functests/utils/profiles"
+	machineconfigv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -50,33 +55,8 @@ var _ = Describe("[rfe_id: 50649]Performance Addon Operator Must Gather", func()
 			var genericFiles = []string{
 				"version",
 				"cluster-scoped-resources/config.openshift.io/featuregates/cluster.yaml",
-				"cluster-scoped-resources/machineconfiguration.openshift.io/kubeletconfigs/performance-performance.yaml",
-				"cluster-scoped-resources/machineconfiguration.openshift.io/machineconfigpools/master.yaml",
-				"cluster-scoped-resources/machineconfiguration.openshift.io/machineconfigpools/worker.yaml",
-				"cluster-scoped-resources/performance.openshift.io/performanceprofiles/performance.yaml",
 				"namespaces/openshift-cluster-node-tuning-operator/tuned.openshift.io/tuneds/default.yaml",
 				"namespaces/openshift-cluster-node-tuning-operator/tuned.openshift.io/tuneds/rendered.yaml",
-				"namespaces/openshift-performance-addon-operator/openshift-performance-addon-operator.yaml",
-				"namespaces/openshift-performance-addon-operator/apps.openshift.io/deploymentconfigs.yaml",
-				"namespaces/openshift-performance-addon-operator/apps/daemonsets.yaml",
-				"namespaces/openshift-performance-addon-operator/apps/deployments.yaml",
-				"namespaces/openshift-performance-addon-operator/apps/replicasets.yaml",
-				"namespaces/openshift-performance-addon-operator/apps/statefulsets.yaml",
-				"namespaces/openshift-performance-addon-operator/autoscaling/horizontalpodautoscalers.yaml",
-				"namespaces/openshift-performance-addon-operator/batch/cronjobs.yaml",
-				"namespaces/openshift-performance-addon-operator/batch/jobs.yaml",
-				"namespaces/openshift-performance-addon-operator/build.openshift.io/buildconfigs.yaml",
-				"namespaces/openshift-performance-addon-operator/build.openshift.io/builds.yaml",
-				"namespaces/openshift-performance-addon-operator/core/configmaps.yaml",
-				"namespaces/openshift-performance-addon-operator/core/endpoints.yaml",
-				"namespaces/openshift-performance-addon-operator/core/events.yaml",
-				"namespaces/openshift-performance-addon-operator/core/persistentvolumeclaims.yaml",
-				"namespaces/openshift-performance-addon-operator/core/pods.yaml",
-				"namespaces/openshift-performance-addon-operator/core/replicationcontrollers.yaml",
-				"namespaces/openshift-performance-addon-operator/core/secrets.yaml",
-				"namespaces/openshift-performance-addon-operator/core/services.yaml",
-				"namespaces/openshift-performance-addon-operator/image.openshift.io/imagestreams.yaml",
-				"namespaces/openshift-performance-addon-operator/route.openshift.io/routes.yaml",
 			}
 			err := checkfilesExist(genericFiles)
 			Expect(err).To(BeNil())
@@ -84,16 +64,46 @@ var _ = Describe("[rfe_id: 50649]Performance Addon Operator Must Gather", func()
 
 		It("[test_id: 50651][crit:high][vendor:cnf-qe@redhat.com][level:acceptance] Verify PAO cluster resources are captured", func() {
 			profile, _ = profiles.GetByNodeLabels(testutils.NodeSelectorLabels)
-			pod, err := pods.GetPerformanceOperatorPod()
-			Expect(err).ToNot(HaveOccurred(), "Failed to find the Performance Addon Operator pod")
-			clusterSpecificFiles := []string{
-				fmt.Sprintf("cluster-scoped-resources/machineconfiguration.openshift.io/machineconfigpools/%s.yaml", testutils.RoleWorker),
-				fmt.Sprintf("namespaces/openshift-cluster-node-tuning-operator/tuned.openshift.io/tuneds/openshift-node-performance-%s.yaml", profile.Name),
-				fmt.Sprintf("namespaces/openshift-performance-addon-operator/pods/%s/%s.yaml", pod.Name, pod.Name),
-				fmt.Sprintf("namespaces/openshift-performance-addon-operator/pods/%s/performance-operator/performance-operator/logs/current.log", pod.Name),
+			if profile != nil {
+				clusterSpecificFiles := []string{
+					fmt.Sprintf("cluster-scoped-resources/machineconfiguration.openshift.io/kubeletconfigs/performance-%s.yaml", profile.Name),
+					fmt.Sprintf("cluster-scoped-resources/performance.openshift.io/performanceprofiles/%s.yaml", profile.Name),
+					fmt.Sprintf("cluster-scoped-resources/machineconfiguration.openshift.io/machineconfigpools/%s.yaml", testutils.RoleWorker),
+					fmt.Sprintf("namespaces/openshift-cluster-node-tuning-operator/tuned.openshift.io/tuneds/openshift-node-performance-%s.yaml", profile.Name),
+				}
+				err = checkfilesExist(clusterSpecificFiles)
+				Expect(err).To(BeNil())
 			}
-			err = checkfilesExist(clusterSpecificFiles)
-			Expect(err).To(BeNil())
+			pod, err := pods.GetPerformanceOperatorPod()
+			if err == nil {
+				paoSpecificFiles := []string{
+					"namespaces/openshift-performance-addon-operator/apps.openshift.io/deploymentconfigs.yaml",
+					"namespaces/openshift-performance-addon-operator/openshift-performance-addon-operator.yaml",
+					"namespaces/openshift-performance-addon-operator/apps/daemonsets.yaml",
+					"namespaces/openshift-performance-addon-operator/apps/deployments.yaml",
+					"namespaces/openshift-performance-addon-operator/apps/replicasets.yaml",
+					"namespaces/openshift-performance-addon-operator/apps/statefulsets.yaml",
+					"namespaces/openshift-performance-addon-operator/autoscaling/horizontalpodautoscalers.yaml",
+					"namespaces/openshift-performance-addon-operator/batch/cronjobs.yaml",
+					"namespaces/openshift-performance-addon-operator/batch/jobs.yaml",
+					"namespaces/openshift-performance-addon-operator/build.openshift.io/buildconfigs.yaml",
+					"namespaces/openshift-performance-addon-operator/build.openshift.io/builds.yaml",
+					"namespaces/openshift-performance-addon-operator/core/configmaps.yaml",
+					"namespaces/openshift-performance-addon-operator/core/endpoints.yaml",
+					"namespaces/openshift-performance-addon-operator/core/events.yaml",
+					"namespaces/openshift-performance-addon-operator/core/persistentvolumeclaims.yaml",
+					"namespaces/openshift-performance-addon-operator/core/pods.yaml",
+					"namespaces/openshift-performance-addon-operator/core/replicationcontrollers.yaml",
+					"namespaces/openshift-performance-addon-operator/core/secrets.yaml",
+					"namespaces/openshift-performance-addon-operator/core/services.yaml",
+					"namespaces/openshift-performance-addon-operator/image.openshift.io/imagestreams.yaml",
+					"namespaces/openshift-performance-addon-operator/route.openshift.io/routes.yaml",
+					fmt.Sprintf("namespaces/openshift-performance-addon-operator/pods/%s/%s.yaml", pod.Name, pod.Name),
+					fmt.Sprintf("namespaces/openshift-performance-addon-operator/pods/%s/performance-operator/performance-operator/logs/current.log", pod.Name),
+				}
+				err = checkfilesExist(paoSpecificFiles)
+				Expect(err).To(BeNil())
+			}
 		})
 		It("[test_id:50652][crit:high][vendor:cnf-qe@redhat.com][level:acceptance] Verify hardware related information are captured", func() {
 			workerRTNodes, err = nodes.GetByLabels(testutils.NodeSelectorLabels)
@@ -113,12 +123,15 @@ var _ = Describe("[rfe_id: 50649]Performance Addon Operator Must Gather", func()
 
 		})
 		It("[test_id:50653][crit:high][vendor:cnf-qe@redhat.com][level:acceptance] Verify machineconfig resources are captured", func() {
-			mcpList := []string{"master", "worker", testutils.RoleWorkerCNF}
-			mcpFiles := make([]string, len(mcpList))
-			for _, mcp := range mcpList {
-				mcpFiles = append(mcpFiles, fmt.Sprintf("cluster-scoped-resources/machineconfiguration.openshift.io/machineconfigpools/%s.yaml", mcp))
+			mcps := &machineconfigv1.MachineConfigPoolList{}
+			selector := labels.NewSelector()
+			err := testclient.Client.List(context.TODO(), mcps, &client.ListOptions{LabelSelector: selector})
+			Expect(err).ToNot(HaveOccurred())
+			mcpFiles := make([]string, len(mcps.Items))
+			for _, item := range mcps.Items {
+				mcpFiles = append(mcpFiles, fmt.Sprintf("cluster-scoped-resources/machineconfiguration.openshift.io/machineconfigpools/%s.yaml", item.Name))
 			}
-			err := checkfilesExist(mcpFiles)
+			err = checkfilesExist(mcpFiles)
 			Expect(err).To(BeNil())
 		})
 
