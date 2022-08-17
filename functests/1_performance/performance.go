72fae79a9c8da70f4c755156498018ec90cd9e3f
shai oren <shaioren@acs48-f01.mgmt.pnr.lab.eng.rdu2.redhat.com>
2022-08-17 09:38:25 +0300

    Add link to the bug

--- functests/1_performance/performance.go
+++ functests/1_performance/performance.go
@@ -1050,6 +1050,7 @@ var _ = Describe("[rfe_id:27368][performance]", func() {
 
 	Context("Verify containers specifications", func() {
 
+		// Automates https://bugzilla.redhat.com/show_bug.cgi?id=2055019
 		It("imagePullPolicy should be ifNotPresent", func() {
 			pod, err := pods.GetPerformanceOperatorPod()
 			Expect(err).ToNot(HaveOccurred())
